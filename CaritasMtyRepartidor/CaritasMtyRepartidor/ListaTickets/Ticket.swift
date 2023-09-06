f3568e872225f70bedadd61815af936e12302a13
dani-suko <a01284684@tec.mx>
2023-09-06 12:11:00 -0600

    pepe

--- CaritasMtyRepartidor/CaritasMtyRepartidor/ListaTickets/Ticket.swift
+++ CaritasMtyRepartidor/CaritasMtyRepartidor/ListaTickets/Ticket.swift
@@ -12,11 +12,13 @@ struct Ticket: Identifiable{
     var nombre : String
     var cantidad : Float
     var estado : Bool
+    var direccion: String
     
-    init(id: Int,nombre: String, cantidad: Float, estado: Bool) {
+    init(id: Int,nombre: String, cantidad: Float, estado: Bool, direccion: String) {
         self.id = id
         self.nombre = nombre
         self.cantidad = cantidad
         self.estado = estado
+        self.direccion = direccion
     }
 }
