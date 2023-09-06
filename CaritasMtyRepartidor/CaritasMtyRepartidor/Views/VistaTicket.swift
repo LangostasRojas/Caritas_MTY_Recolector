71ec55abd7e2b91340ff343adbe062cddd9e4064
Kraken Alejandro Dominguez Ramirez <142059157+KrakenDominguezz@users.noreply.github.com>
2023-09-06 11:35:15 -0600

    Hecho Listas con formato de tickets

--- CaritasMtyRepartidor/CaritasMtyRepartidor/Views/VistaTicket.swift
+++ CaritasMtyRepartidor/CaritasMtyRepartidor/Views/VistaTicket.swift
@@ -5,16 +5,85 @@
 //  Created by Alumno on 06/09/23.
 //
 
+//
+//  VistasDetailed.swift
+//  CaritasMty
+//
+//  Created by Alumno on 06/09/23.
+//
+
 import SwiftUI
 
 struct VistaTicket: View {
-        var ticket : Ticket
-        var body: some View {
-            Text("Ticket")
-            Text("ID: \(ticket.id)")
-            Text("Nombre: \(ticket.nombre)")
-            Text("cantidad: \(ticket.cantidad)")
+    var ticket : Ticket
+
+    var body: some View {
+        
+        ZStack{
+            
+            Color("BgColor")
+                .ignoresSafeArea()
+            VStack{
+                Text("Ticket - \(ticket.id)")
+                    .foregroundColor(.white)
+                    .font(.system(size: 40))
+                    .fontWeight(.bold)
+                    .padding(.trailing, 100.0)
+                Rectangle()
+                    .fill(Color("ColorAzulVerdePaleta"))
+                    .frame(width: 170,height: 7)
+                    .cornerRadius(20)
+                    .offset(x:-60,y:-25)
+                Image("Mapa")
+                    .resizable(resizingMode: .stretch)
+                    .aspectRatio(contentMode: .fit)
+                    .frame(width: 250)
+
+                    VStack{
+                        Group{
+                            Text("Nombre: ")
+                                .padding(10)
+                                .fontWeight(.bold)
+                            Text(ticket.nombre)
+                                .padding(.bottom,5.0)
+                            Text("Dirección")
+                                .padding(.bottom,5.0)
+                                .fontWeight(.bold)
+                            Text("Calle Eugenio Garza Sada, Lomas Garza, Boca del Rio, Veracruz, C.P. 95264, Mexico")
+                            Text("Cantidad")
+                                .padding(.vertical,5)
+                                .fontWeight(.bold)
+                            Text("$ \(String(format: "%.2f", ticket.cantidad))")
+                                .padding(.bottom,10)
+                        }
+                        .font(.system(size: 20))
+                        .padding(.horizontal,20.00)
+                       
+                    }
+                    .foregroundColor(.white)
+
+                Button(action: {
+                    
+                }, label: {
+                    Text("Completar Entrega")
+                        .font(.title2)
+                        .bold()
+                        .foregroundColor(.white)
+                    
+                        .frame(width: 280.0, height: 70.0)
+                        .background(Color("ColorAzulVerdePaleta"))
+                        .cornerRadius(30)
+                        .shadow(color:.black,radius: 2,y:2)
+                })
+
+            
+                
+                    
+            }
+           
+            
         }
+    }
 }
 
 struct VistaTicket_Previews: PreviewProvider {
@@ -22,3 +91,4 @@ struct VistaTicket_Previews: PreviewProvider {
         VistaTicket(ticket: listaTickets[0])
     }
 }
+
